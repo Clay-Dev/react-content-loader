6ded6bdbb2a79989abbf58169eb7aaf6f284d99d
Maxim Dubrovin <maxxdubrovin@gmail.com>
2018-05-14 20:55:23 +0500

    test commit: accept style for Circle

--- src/custom/Circle.js
+++ src/custom/Circle.js
@@ -5,11 +5,12 @@ type Props = {
   x: number,
   y: number,
   radius: number,
+  style: object
 }
 
 const Circle = (props: Props): React.Element<*> => {
-  const { x = 0, y = 0, radius = 50 } = props
-  return <circle cx={x} cy={y} r={radius} />
+  const { x = 0, y = 0, radius = 50, style = {} } = props
+  return <circle cx={x} cy={y} r={radius} style={style} />
 }
 
 export default Circle
